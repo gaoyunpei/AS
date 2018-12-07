4828ba14cea9dd02fc83601f4a6d1d0b941c47e5
gaoyunpei <gaoyunpei@163.com>
2018-12-07 17:30:17 +0800

    AS project for dialog demo.

---
+++ DialogDemo/app/src/main/java/com/example/gaoyunpei/dialogdemo/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.gaoyunpei.dialogdemo;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.content.DialogInterface;
+import android.app.AlertDialog;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    showdialog();
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    public void showdialog()
+    {
+        //Toast.makeText(this,"clickme",Toast.LENGTH_LONG).show();
+        AlertDialog.Builder alertdialogbuilder=new AlertDialog.Builder(this);
+        alertdialogbuilder.setMessage("您确认要退出程序");
+        alertdialogbuilder.setPositiveButton("确定", click1);
+        alertdialogbuilder.setNegativeButton("取消", click2);
+        alertdialogbuilder.setNeutralButton("ok",button1);
+        AlertDialog alertdialog1=alertdialogbuilder.create();
+        alertdialog1.show();
+    }
+
+    private DialogInterface.OnClickListener click1=new DialogInterface.OnClickListener()
+    {   @Override
+        public void onClick(DialogInterface arg0,int arg1)
+        {
+            android.os.Process.killProcess(android.os.Process.myPid());
+        }
+    };
+    private DialogInterface.OnClickListener click2=new DialogInterface.OnClickListener()
+    {
+        @Override
+        public void onClick(DialogInterface arg0,int arg1)
+        {
+            arg0.cancel();
+        }
+    };
+    private DialogInterface.OnClickListener button1=new DialogInterface.OnClickListener()
+    {
+        @Override
+        public void onClick(DialogInterface arg0,int arg1)
+        {
+            Toast.makeText(getApplicationContext(),"Toast test.",Toast.LENGTH_SHORT).show();
+        }
+    };
+}
